99c1147a90c3440b743ad4ba06d95cde8af6304d
arch_ap_007 <rockcong@gmail.com>
2025-04-27 14:33:10 +0800

    add several case and let them running test

---
+++ src/main/scala/simple/Adder4Bit.scala
@@ -0,0 +1,20 @@
+package simple
+
+import chisel3._
+import firrtl.stage.FirrtlStage
+
+class Adder4Bit extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(4.W))
+    val b = Input(UInt(4.W))
+    val sum = Output(UInt(4.W))
+  })
+
+  io.sum := io.a + io.b
+}
+
+
+
+object Adder4BitApp extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog(new Adder4Bit(),Array("--target-dir","generated"))
+}
